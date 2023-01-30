87b5f78f634d9fcb4f714f9307938ed3eb038719
MAME SANDECK NIANG <63318185+NIANG95@users.noreply.github.com>
2023-01-30 20:18:55 +0000

    Update App.java

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Mame!" );
+        System.out.println( "Hello World from Mame Sandeck Niang!" );
     }
 }
